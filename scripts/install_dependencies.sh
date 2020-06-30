93117eec06d95d18bd4b8ff34e437bffa7f7fafe
p-karol <priscilla.karolczak@gmail.com>
2020-06-30 20:19:47 +0100

    corrigido start_server

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -9,8 +9,6 @@ sudo locale-gen pt_BR.UTF-8
 sudo locale-gen en_US.UTF-8
 export LC_ALL="en_US.UTF-8"
 export LC_CTYPE="en_US.UTF-8"
-sudo dpkg-reconfigure locales
-
 
 sudo apt-get update
 sudo apt-get install -y python2.7 python-pip virtualenv
