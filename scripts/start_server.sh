b519d5771ce93156521affaf2a0ff87b7084d934
p-karol <priscilla.karolczak@gmail.com>
2020-06-30 19:14:21 +0100

    editado dependencias

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -6,9 +6,7 @@ VIRTUALENV=/home/ubuntu/venv
 pushd $(dirname "$0")
 source $VIRTUALENV/bin/activate
 export FLASK_APP=app.py
-flask run --host=0.0.0.0 --port 8080 >/dev/null 2>&1 &
-deactivate
-popd
+/usr/local/bin/flaskflask run --host=0.0.0.0 --port 8080 >/dev/null 2>&1 &
 
 
 #cd /home/ubuntu
