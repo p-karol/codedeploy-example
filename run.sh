efe1720ac08de300fb8da6b6218864d05166b0c5
p-karol <priscilla.karolczak@gmail.com>
2020-06-30 14:02:05 +0100

    alterada app

--- run.sh
+++ run.sh
@@ -6,11 +6,6 @@ VIRTUALENV=/home/ubuntu/venv
 pushd $(dirname "$0")
 source $VIRTUALENV/bin/activate
 export FLASK_APP=app.py
-flask run --host=0.0.0.0 --port 8080 >/dev/null 2>&1 &
+flask run --host=0.0.0.0 --port 8080
 deactivate
 popd
-
-
-#cd /home/ubuntu
-#export FLASK_APP=hello.py
-#flask run --host=0.0.0.0 --port 8080 >/dev/null 2>&1 &
