f8b11d7a4abd7eb5f1d4f79b3e35f456d22c8674
p-karol <priscilla.karolczak@gmail.com>
2020-06-30 18:57:40 +0100

    editado dependencias

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-#sudo locale-gen pt_BR.UTF-8
-#export LC_ALL="en_US.UTF-8"
-#export LC_CTYPE="en_US.UTF-8"
+sudo locale-gen pt_BR.UTF-8
+export LC_ALL="en_US.UTF-8"
+export LC_CTYPE="en_US.UTF-8"
 #sudo dpkg-reconfigure locales
 
 sudo apt-get update
 sudo apt-get install -y python2.7 python-pip virtualenv
 
 mkdir venv
-virtualenv venv
+/usr/bin/virtualen venv
 source venv/bin/activate
 pip install Flask flask-wtf boto3
