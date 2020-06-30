1b65a36bc7082c9bac978d4f0b111283e36c2209
p-karol <priscilla.karolczak@gmail.com>
2020-06-30 19:07:49 +0100

    editado dependencias

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -9,6 +9,6 @@ sudo apt-get update
 sudo apt-get install -y python2.7 python-pip virtualenv
 
 mkdir /home/ubuntu/venv
-/usr/bin/virtualen /home/ubuntu/venv
+/usr/bin/virtualenv /home/ubuntu/venv
 source /home/ubuntu/venv/bin/activate
 pip install Flask flask-wtf boto3
