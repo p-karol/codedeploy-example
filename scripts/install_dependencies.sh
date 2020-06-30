77c2edb1e8180aada348914d03de25508caae6c0
p-karol <priscilla.karolczak@gmail.com>
2020-06-30 18:19:19 +0100

    editado dependencias

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-sudo locale-gen pt_BR.UTF-8
-export LC_ALL="en_US.UTF-8"
-export LC_CTYPE="en_US.UTF-8"
-sudo dpkg-reconfigure locales
+#sudo locale-gen pt_BR.UTF-8
+#export LC_ALL="en_US.UTF-8"
+#export LC_CTYPE="en_US.UTF-8"
+#sudo dpkg-reconfigure locales
 
 sudo apt-get update
 sudo apt-get install -y python2.7 python-pip virtualenv
