89217957c953a72a956a47523ad5fe9d0741cb3e
p-karol <priscilla.karolczak@gmail.com>
2020-06-30 16:02:48 +0100

    install dependencies editado

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-sudo apt-get update
-sudo apt-get install -y python2.7 python-pip virtualenv
-
 sudo locale-gen pt_BR.UTF-8
 export LC_ALL="en_US.UTF-8"
 export LC_CTYPE="en_US.UTF-8"
 sudo dpkg-reconfigure locales
+
+sudo apt-get update
+sudo apt-get install -y python2.7 python-pip virtualenv
 
 mkdir venv
 virtualenv venv
